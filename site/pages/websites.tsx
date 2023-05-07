471a5cfe37f3019cb8db41d5becc39c1545e35c4
Mose Run <moserun@Moses-MacBook-Air.local>
2023-05-07 17:48:43 -0400

    added tags to data and working on iterating over it

--- site/pages/websites.tsx
+++ site/pages/websites.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import Navbar from "@/components/Navbar";
 import ToolCard from "@/components/ToolCard";
 import { useRouter } from "next/router";
-import data from '../catalog.json'
+import data from "../catalog.json";
 
 interface Tool {
   name: string;
@@ -11,21 +11,30 @@ interface Tool {
   link: string;
 }
 
-const Catalogue = () => {
-  const router = useRouter()
-  const { catagory } = router.query
-  console.log(catagory)
-  
-  return (
-    <div className='catalog-page'>
-      <Navbar />
-      <h1 className='catalogue-header'>Explore</h1>
 
-      <main className='catalogue'>
-        
-        {data.map((tool: Tool, index: number) => (
+const Catalogue = () => {
+  const router = useRouter();
+  const { catagory } = router.query;
+  console.log(catagory);
+
+  data.forEach((tags) => {
+    tags.tag?.forEach((tag) => {
+      if (tag === catagory) {
+        console.log(tag);
+      }
+    });
+  });
+
+  return (
+    <div className="catalog-page">
+      <Navbar />
+      <h1 className="catalogue-header">Explore</h1>
+
+      <main className="catalogue">
+        {/* {data.map((tool: Tool, index: number) => (
           
           <ToolCard 
+          
             key={index}
             name={tool.name}
             paragraph={tool.paragraph}
@@ -34,10 +43,41 @@ const Catalogue = () => {
             link={tool.link}
           />
           
-        ))}
+        ))} */}
+
+        {data.map((tool, index: number) => {
+
+          console.log(tool.tag?.forEach((tag) => {
+            if (tag === catagory) {
+           
+            }
+          }))
+          tool.tag?.forEach((tag) => {
+            if (tag === catagory) {
+              console.log(tag)
+            }
+          })
+          return (
+            <ToolCard 
+          
+              key={index}
+              name={tool.name}
+              paragraph={tool.paragraph}
+              imgSrc={tool.imgLink}
+              imgAlt='image'
+              link={tool.link}
+            />
+          )
+        })}
+
+        {data.map((tool, index:number) => {
+          tool.tag?.forEach((tag) => {
+            if
+          })
+        })}
       </main>
     </div>
-  )
-}
+  );
+};
 
 export default Catalogue;
